4985f7f136e15946dfb3cff8f448c85e45f60c92
Ryan McHenry <ryan@plays.tv>
2018-08-12 07:22:36 -0400

    trying another config

--- src/lib/socket-server.js
+++ src/lib/socket-server.js
@@ -1,7 +1,6 @@
+const PORT = process.env.PORT || 5000;
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-
-const PORT = process.env.PORT || 3000;
 
 io.on('connection', function (client) {
   console.log('connect');
