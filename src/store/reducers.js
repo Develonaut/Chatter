6bc45a97a307c19323ed21648b69ee6f313b1953
Ryan McHenry <ryan@plays.tv>
2018-08-12 05:57:05 -0400

    Adding module back

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import users from '../modules/UsersModule';
 import messages from '../modules/MessagesModule';
 
 const reducers = combineReducers({
-    users,
     messages,
 })
 
